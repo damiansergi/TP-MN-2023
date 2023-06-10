757702b6ba86e8bd0fa606738a2cc1251f8a7710
damiansergi <72512614+damiansergi@users.noreply.github.com>
2023-06-10 14:45:09 -0300

    arregle E5

--- E5.m
+++ E5.m
@@ -1,7 +1,7 @@
 %La función en la cual se aplica el método de punto fijo es f(t)=t-(12*(1-e^(-0.04t))+5*e^(-0.04t)-10.2)
 
 %Para encontrar un intervalo en donde se encuentre la solución basta con
-%evaluar f(t) = 12*(1-e^(-0.04*(t)))+5*e^(-0.04*(t))-10.2; (donde 10.2 = 0.85*Ce)
+%evaluar f(t) = 12*(1-e^(-0.04*(t)))+5*e^(-0.06*(t))-10.2; (donde 10.2 = 0.85*Ce)
 %y encontrar dos puntos x1, x2 tal que f(x1)*f(x2)<0. Tales podrían ser por ejemplo
 %x1=0 y x2=50
 %Es válido que la raíz sea única en este punto ya que f'(t)>0 para todo t>0
@@ -10,15 +10,15 @@
 
 format long;
 function y=g(t) %Funcion para el metodo de punto fijo
-  y = (t)-(12*(1-e^(-0.04*(t)))+5*e^(-0.04*(t))-10.2);
+  y = (t)-(12*(1-e^(-0.04*(t)))+5*e^(-0.06*(t))-10.2);
 endfunction
 
 function y=f(t) %Funcion para el metodo de Newton-Raphson
-  y = 12*(1-e^(-0.04*(t)))+5*e^(-0.04*(t))-10.2;
+  y = 12*(1-e^(-0.04*(t)))+5*e^(-0.06*(t))-10.2;
 endfunction
 
 function y=df(t)  %Funcion para el metodo de Newton-Raphson
-  y = (0.04)*12*e^(-0.04*(t))+(-0.04)*5*e^(-0.04*(t));
+  y = (0.04)*12*e^(-0.04*(t))+(-0.06)*5*e^(-0.06*(t));
 endfunction
 
 %Parametros para el método de punto fijo
